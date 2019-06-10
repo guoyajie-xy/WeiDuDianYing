19442cf93296020d6912f43914426a8b513a5506
Lenovo <https://github.com/Zhanglihon/asdasdasd.git>
2019-06-10 15:40:06 +0800

    张利宏修改

--- app/src/main/java/com/bw/movie/activity/LoginActivity.java
+++ app/src/main/java/com/bw/movie/activity/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.bw.movie.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -44,6 +45,16 @@ public class LoginActivity extends AppCompatActivity implements DataCall<LoginBe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        //获取sp对象
+        SharedPreferences sharedPreferences=getSharedPreferences("config",MODE_PRIVATE);
+        boolean flag = sharedPreferences.getBoolean("flag", false);
+        loginCheckPwd.setChecked(flag);
+        if (flag){
+            String name=sharedPreferences.getString("name","");
+            String pwd=sharedPreferences.getString("pwd","");
+            loginEditPhone.setText(name);
+            loginEditPwd.setText(pwd);
+        }
         loginPresenter = new LoginPresenter(this);
         //点击快速注册跳转
         loginTextKszc.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +71,15 @@ public class LoginActivity extends AppCompatActivity implements DataCall<LoginBe
             public void onClick(View v) {
                 String name=loginEditPhone.getText().toString().trim();
                 String pwd=loginEditPwd.getText().toString().trim();
+                SharedPreferences.Editor edit=sharedPreferences.edit();
+                if (loginCheckPwd.isChecked()){
+                    edit.putString("name",name);
+                    edit.putString("pwd",pwd);
+                    edit.putBoolean("flag",true);
+                }else {
+                    edit.clear();
+                }
+                edit.commit();
                 if (TextUtils.isEmpty(name)&&TextUtils.isEmpty(pwd)){
                     Toast.makeText(LoginActivity.this,"手机号或密码不能为空",Toast.LENGTH_SHORT).show();
                 }
