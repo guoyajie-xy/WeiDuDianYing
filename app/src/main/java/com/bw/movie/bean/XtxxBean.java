994e22b10a17c62d1a3ce823e626c1a7f2ed61a0
Lenovo <https://github.com/Zhanglihon/asdasdasd.git>
2019-06-12 11:03:30 +0800

    张利宏修改

---
+++ app/src/main/java/com/bw/movie/bean/XtxxBean.java
@@ -0,0 +1,17 @@
+package com.bw.movie.bean;
+
+/**
+ * Time:${Data}
+ * <p>
+ * Author:Lenovo
+ * <p>
+ * Description:写这个类的作用
+ */
+public class XtxxBean {
+    public int id;
+    public String content;
+    public long pushTime;
+    public String status;
+    public String title;
+    public String userId;
+}
